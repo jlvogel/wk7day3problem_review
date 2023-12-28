5d9a621f0c7b9816e5b2ccd2e570a9fb9e9c5b08
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-28 02:42:45 -0500

    continuing to create App.js line by line and add comments.  Understand and test better understanding of the spread (...) operator as it applies to making copies of arrays.  arrayCopy = [...array] makes sure arrayCopy won't change if array does.  arrayCopy = array is not good enough! any changes to array in the last case WILL change arrayCopy as well!

--- src/test.js
+++ src/test.js
@@ -1,8 +1,13 @@
 const todos = [1,2,3]
 
-setTodos = () => {
-  let value = 2
-  return [{key:value}, ...todos]
-}
+// setTodos = () => {
+//   let value = 2
+//   return [{key:value}, ...todos]
+// }
 
-console.dir(setTodos())
+// console.dir(setTodos())
+
+const todosCopy = [...todos]
+
+todos[0] = ":{"
+console.log(todosCopy)
