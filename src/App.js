8e74e7ff45b2eae73b72be370c15576c745e5df0
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-27 22:54:36 -0500

    going through App.js line by line and making sure runs and adding comments as I go to make sure I understand best I can

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import { useState, useEffect } from "react"
 import TodoList from "./components/TodoList"
 
 export default function App() {
+
+  const [todos, setTodos] = useState([])
+  /// todos is the state
+  /// setTodos is the function that will update the state
+  /// initial state value is blank array
+
+  const addTodo = (e) => {
+  /// addTodo is a function that takes in a parameter e
+
+    const newTodo = { text: e.target.value, id: Date.now(), completed: false }
+    /// newTodo is an object with 3 key-value pairs (text, id, and completed properties)
+
+    setTodos([newTodo, ...todos])
+    /// from before setTodos is the function that updates the state todos.  
+
+
+  }
+
   return (
-    <div className="app">
+    <div className="App">
       <TodoList/>
     </div>
   )
