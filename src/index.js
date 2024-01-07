390842c6ed655a4494f04ccf87c803db1455035e
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2024-01-07 02:10:36 -0500

    consolidating console logs - putting as many console logs into one console log as I can.  Scattering console logs throughout the code helped me understand the flow of information better but it has made reading and following the code alot more difficult.  I can now consolidate logs now that I can see the order of execution to make sure I am not putting anything out of sequence when I do so.  This should make it a lot easier to follow along what is happening and will greatly simplify coding the logs.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
-console.log("")
-console.log("START OF FILE index.js")
+console.log(`
+START OF FILE index.js`)
 
-import { StrictMode } from "react"
+console.log(`
+  import { createRoot } from "react-dom/client"
+  import App from "./App"
+
+  const rootElement = document.getElementById("root")
+  const root = createRoot(rootElement)
+
+  root .render(
+    <>
+      <h1>Basic Todo List Evelyn</h1>
+      <App />
+    </>
+  )`)
+
+import { StrictMode } from "react" // currently not used because 
+                                   // tags commented out to avoid duplicate
+                                   // console logs
+
 import { createRoot } from "react-dom/client"
-
 import App from "./App"
 
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
 
-console.log("  START root .render(<></>)") // <> and </> are opening and
-                                           // closing fragment tags.
 root .render(
   // <StrictMode> // Note commenting out these StrictMode tags
                   // gets rid of duplicate console logs.  Not exactly
                   // sure why at them moment - something to look into 
                   // later.
-  <>
-  {console.log("    <>")}
-
-    {console.log("      <h1>Basic Todo List Evelyn</h1>")}
+  <>  {/*<> is a fragment tag*/}
     <h1>Basic Todo List Evelyn</h1>
-
-    {console.log("      <App />")}
     <App />
-
-  {console.log("    </>")}
   </>
   // </StrictMode>
 )
-console.log("  END root .render(<></>)")
 
-console.log("END OF FILE index.js")
+console.log(`
+END OF FILE index.js`)
