630993dbd415f966c5befa1cb0fc9647c15638c5
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2024-01-12 12:56:29 -0500

    cleaned up console log for addTodo function

--- src/App.js
+++ src/App.js
@@ -91,24 +91,23 @@ export default function App() {
 
   const addTodo = (e) => {
   /// addTodo is a function that takes in a parameter e
-    console.log(`  START FUNCTION addTodo(e)`)
-    console.log(`    const newTodo = { text: e.target.value, id: Date.now(), completed: false }`)
     const newTodo = { text: e.target.value, id: Date.now(), completed: false }
     /// newTodo is an object with 3 key-value pairs (text, id, and completed properties)
-    console.log(`    newTodo == `, newTodo)
-    console.log('    todos == ', todos)
-    console.log(`    [newTodo, ...todos] == `,[newTodo, ...todos])
-    console.log(`    setTodos([newTodo, ...todos]) // useState Hook!`)
 
-    setTodos([newTodo, ...todos])
+    setTodos([newTodo, ...todos]) // useState Hook!
     /// from before setTodos is the function that updates the state todos.  Best I can tell right now the state that setTodos will set todos to will be an array with 2 elemnents: newTodo, and ...todos.  todos is the state before setTodos updates it.   ... is the spread operator, which takes the elements in an array and pulls them out individually so the 2nd element could actually be more than one element.  We can see that initially todos is a blank array.   I am creating a test.js to understand this better.  Yep!  That's exactly what it does!
 
-    console.log("    todos == ", todos)
 
-    console.log("    e.target.value = \"\"")
     e.target.value = "" /// sets the target to empty string
 
-    console.log("  END FUNCTION addTodo(e)")
+  console.log(`
+    const addTodo = (e) => {
+        const newTodo = { text: e.target.value, id: Date.now(), completed: false }
+        setTodos([newTodo, ...todos]) // useState Hook!
+        e.target.value = "" /// sets the target to empty string
+    }
+  `)
+
   }
 
   const completeTodo = (id, e) => {
